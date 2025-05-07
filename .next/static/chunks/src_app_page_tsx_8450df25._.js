99016798fe72813864159face973c579647be30c
Billard Timothe <timothe.billard@gmail.com>
2025-05-07 03:27:31 +0200

    Nettoyage de l’index et prise en compte du .gitignore

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_0b60bc78._.js",
-  "static/chunks/src_app_d9f2b31b._.js"
-],
-    source: "dynamic"
-});
